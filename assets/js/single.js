3b5703e35c9df3a08f531427ae1df1b92314a00f
Vox Humana <jonprine@gmail.com>
2020-07-08 11:56:11 -0500

    repo name / issues listed at top of page and redirect to index if not repos listed in search

--- assets/js/single.js
+++ assets/js/single.js
@@ -4,25 +4,44 @@ var issueContainerEl = document.querySelector("#issues-container");
 // 30 repo limit
 var limitWarningEl = document.querySelector("#limit-warning");
 
+// add repo name to the header
+var repoNameEl = document.querySelector("#repo-name");
+
+// splitting repo name from string
+getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
 
 
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function (response) {
         // request was successful
         if (response.ok) {
             response.json().then(function (data) {
-                // pass response data to dom function
                 displayIssues(data);
+
                 // check if api has paginated issues
                 if (response.headers.get("Link")) {
                     displayWarning(repo);
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 
@@ -74,9 +93,9 @@ var displayWarning = function (repo) {
     linkEl.textContent = "See More Issues on GitHub.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
     linkEl.setAttribute("target", "_blank");
-  
+
     // append to warning container
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
